9fd6e6f9a1108fe105070f32a3674b1a4658079d
Maxim <maxim2k18gh8hg@gmail.com>
2024-09-08 08:35:40 +0300

    upd hw 5

--- hw_5/task_1.cpp
+++ hw_5/task_1.cpp
@@ -3,48 +3,6 @@ using namespace std;
 
 int main()
 {
-    srand(time(0));
-
-    const int rows = 4;
-    const int cols = 7;
-
-    int matrix[rows][cols];
-    int sum = 0;
-    int maxSumRow = 0;
-    int maxsum = 0;
-
-
-    for (int i = 0; i < rows; i++)
-    {
-        sum = 0;
-        for (int j = 0; j < cols; j++)
-        {
-            matrix[i][j] = rand() % 90 + 10;
-            sum += matrix[i][j];
-            cout << matrix[i][j] << " ";
-
-        }
-        cout << " " << sum << endl;
-
-        if (sum > maxsum)
-        {
-            maxsum = sum;
-            maxSumRow = i;
-        }
-    }
-
-    cout << "Max row: " << maxSumRow + 1 << " (sum: " << maxsum << ")" << endl;
-}
-
-
-
-
-
-#include <iostream>
-using namespace std;
-
-int main()
-{
     const int size = 10;
 
     int mas[size]{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
