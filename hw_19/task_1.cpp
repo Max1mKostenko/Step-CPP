9e36b7a191284ef2ca3162e1b3566c4675ebf5e3
Maxim <maxim2k18gh8hg@gmail.com>
2024-10-26 21:23:59 +0300

    upd hw 19

--- hw_19/task_1.cpp
+++ hw_19/task_1.cpp
@@ -1,36 +1,30 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 using namespace std;
 
 int main() {
-    ofstream createFile("input.txt");
-    createFile << "Hello everyone, One Two Three." << endl;
-    createFile.close();
+    ofstream fout("text.txt");
+    fout << "Съешь же ещё этих мягких французских булок, да выпей чаю.";
+    fout.close();
 
-    string word_to_find, word_to_replace;
+    int shift = 3;
+    char line[200];
 
-    cout << "Enter the word to find: ";
-    cin >> word_to_find;
-    cout << "Enter the word to replace: ";
-    cin >> word_to_replace;
+    ifstream fin("text.txt");
 
-    ifstream fin("input.txt");
-    ofstream fout("output.txt");
-    string line;
-
-    while (getline(fin, line)) {
-        int pos = line.find(word_to_find);
-
-        while (pos >= 0 && pos < line.length()) {
-            line.replace(pos, word_to_find.length(), word_to_replace);
-            pos = line.find(word_to_find, pos + word_to_replace.length());
+    while (fin.getline(line, 200)) {
+        for (int i = 0; line[i] != '\0'; i++) {
+            char c = line[i];
+            if (c >= 'А' && c <= 'Я') {
+                line[i] = 'А' + (c - 'А' + shift) % 32;
+            }
+            else if (c >= 'а' && c <= 'я') {
+                line[i] = 'а' + (c - 'а' + shift) % 32;
+            }
         }
         fout << line << endl;
     }
 
     fin.close();
-    fout.close();
 
-    cout << "Process complete. Check the output file" << endl;
 }
